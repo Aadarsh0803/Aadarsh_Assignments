f9ccb2cc4d4cb6dce280b2675fa03828b2fa925f
Aadarsh <128207477+Aadarsh0803@users.noreply.github.com>
2024-07-24 15:12:34 +0530

    Commit

---
+++ TempConvLibrary/TempConvLibrary/TempConvert.cs
@@ -0,0 +1,15 @@
+﻿namespace TempConvLibrary
+{
+    public class TempConvert
+    {
+        public double CTF(double temp)
+        {
+            double result = ((9*temp)/5)+32;
+            return result;
+        }
+        public double FTC(double temp)
+        {
+            return (temp - 32) * 5 / 9;
+        }
+    }
+}
---
+++ TempConvLibrary/TempProject/Program.cs
@@ -0,0 +1,14 @@
+﻿/*using TempConvLibrary;
+namespace TempProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConvert a = new TempConvert();
+            Console.WriteLine(a.FTC(32));
+
+        }
+    }
+}
+*/
